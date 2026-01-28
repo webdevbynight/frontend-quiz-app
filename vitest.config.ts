e84e37338a8b3146d2cfd505dc2f49721ca38870
webdevbynight <git@victor-brito.dev>
2026-01-28 22:59:20 +0100

    Set up Vitest

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    silent: true,
+    typecheck: {
+      enabled: true,
+      tsconfig: "./tsconfig.test.json",
+      include: ["test/**/*.test.ts"]
+    },
+    environment: "jsdom"
+  }
+});
